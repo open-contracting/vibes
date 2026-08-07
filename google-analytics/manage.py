342146f6458410570b606177107d06ff4589f34e
James McKinney <26463+jpmckinney@users.noreply.github.com>
2026-08-06 23:35:44 -0400

    chore: Use python3 interpreter (python not found on macOS)

--- google-analytics/manage.py
+++ google-analytics/manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 GA4 peak-load reporter.
 
--- onedrive-downloader/manage.py
+++ onedrive-downloader/manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Download all files from a OneDrive personal shared folder (recursively).
 
